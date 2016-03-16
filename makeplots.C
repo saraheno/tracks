fc7534a086c2c55e58d3693011778a7d7f64e1c9
Sarah <eno@umd.edu>
2016-03-16 08:49:48 -0400

    ha

--- makeplots.C
+++ makeplots.C
@@ -4,10 +4,14 @@ void makeplots(){
   gStyle->SetOptStat(111111);
 
   TH1F *hrgen2 = (TH1F*)f1->Get("SCETracks/hrgen2");
-  //TH1F *hratvr = (TH1F*)f1->Get("hratvr");
+  TH1F *hrreco = (TH1F*)f1->Get("SCETracks/hrreco");
 
+  hrgen2->SetLineWidth(2);
+  hrgen2->SetLineColor(2);
+  hrreco->SetLineWidth(2);
+  hrreco->SetLineColor(3);
 
 
  hrgen2->Draw();
- // hratvr->Draw("same");
+ hrreco->Draw("same");
 }
